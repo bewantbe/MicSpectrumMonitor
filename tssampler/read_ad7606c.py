6bb9b50145767cc48a7573c06b5b373dcfc708c1
xyyxle <bewantbe@gmail.com>
2024-01-23 06:25:12 +0800

    Add control to voltage range

--- tssampler/read_ad7606c.py
+++ tssampler/read_ad7606c.py
@@ -15,16 +15,17 @@ class AD7606CReader(tssabc.SampleReader):
     def __init__(self):
         self.initilized = False
 
-    def init(self, sample_rate, periodsize=None, stream_callback=None, **kwargs):
+    def init(self, sample_rate, periodsize=None, stream_callback=None, volt_range=[-2.5, 2.5], ends='single'):
         self.chunk_size = periodsize           # TODO: are they the same
         self.adc = M3F20xmADC(reset = True)
+        self.adc.set_input_range(volt_range, ends)
         self.adc.set_sampling_rate(sample_rate)
         self.n_channels = self.adc.n_channels
         self.sample_rate = 1.0 / self.adc.get_sampling_interval()
         self.adc.start()
         self.initilized = True
         return self
-
+    
     def read(self, n_frames):
         n_frames_left = self.adc.get_fifo_frames_left()
         t_wait = (2 * n_frames - n_frames_left) / self.sample_rate
@@ -48,7 +49,9 @@ if __name__ == '__main__':
     
     # test the class
     mic_reader = AD7606CReader()
-    mic_reader.init(48000)
+    mic_reader.init(48000, volt_range=[-2.5, 2.5], ends='single')
+    mic_reader.adc.show_config()
+    mic_reader.adc.show_reg()
     vals = [None] * (6*2)
     for i in range(len(vals)):
         vals[i] = mic_reader.read(8000)
