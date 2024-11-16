7ae61dda0574afa6c4c899a9df1e8cff137949c6
xyyxle <bewantbe@gmail.com>
2024-11-17 00:26:14 +0800

    Fix calibration code for OSCA02

--- audiospectra/tssampler/scope_osca02.py
+++ audiospectra/tssampler/scope_osca02.py
@@ -18,7 +18,8 @@ logger = logging.getLogger('lotoosc')
 if __name__ == '__main__':
     import tssabc     # use as script
     # enable logging to info level
-    logging.basicConfig(level=logging.DEBUG)
+    #logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.INFO)
 else:
     from . import tssabc    # use as lib
     logger.setLevel(logging.CRITICAL)
@@ -72,7 +73,9 @@ def sampling_rate_normalization(sr_request, g_CtrlByte0):
     g_CtrlByte0 |= sr_cmdx[sr_idx]
     return sr_list[sr_idx], g_CtrlByte0
 
-osc_volt_series = [8.0, 5.0, 2.5, 1.0, 0.5, 0.25, 0.1]
+#osc_volt_series = [8.0, 5.0, 2.5, 1.0, 0.5, 0.25, 0.1]
+osc_volt_series = [10.0, 5.0, 2.5, 1.0, 0.5, 0.25, 0.1]
+osc_vdiv_series = [2.0, 1.0, 0.5, 0.2, 0.1, 0.05, 0.02]
 
 def set_volt_range(va_request, vb_request, g_CtrlByte1):
     v_list = np.array(osc_volt_series)
@@ -81,7 +84,7 @@ def set_volt_range(va_request, vb_request, g_CtrlByte1):
     mask_a_t   = [0x00, 0x02, 0x04, 0x06, 0x02, 0x04, 0x06]
     mask_b_and = 0xF9
     mask_b_or  = [0x00, 0x02, 0x04, 0x06, 0x02, 0x04, 0x06]
-    mask_b_t   = [0x00, 0x00, 0x00, 0x00, 0x04, 0x04, 0x04]
+    mask_b_t   = [0x00, 0x00, 0x00, 0x00, 0x40, 0x40, 0x40]
     # if the request is out of range, raise error
     if va_request > v_list.max():
         raise ValueError(f"va_request {va_request} is out of range")
@@ -100,11 +103,12 @@ def set_volt_range(va_request, vb_request, g_CtrlByte1):
     g_CtrlByte1 |= mask_b_or[vb_idx]
     USBCtrlTrans(c_ubyte(0x23), c_ushort(mask_b_t[vb_idx]), c_ulong(1))
     USBCtrlTrans(c_ubyte(0x24), c_ushort(g_CtrlByte1), c_ulong(1))
+    time.sleep(0.1)
     return v_list[va_idx], v_list[vb_idx], va_idx, vb_idx, g_CtrlByte1
 
 def read_volt_calib_data():
     #           A     B
-    zvcmd = [0x01, 0x02,  # 8
+    zvcmd = [0x82, 0x72,  # 8
              0x01, 0x02,  # 5
              0x0E, 0x0F,  # 2.5
              0x14, 0x15,  # 1
@@ -114,8 +118,6 @@ def read_volt_calib_data():
     zero_volt_offset = np.zeros((7, 2), dtype=np.float64)
     for j, cmd in enumerate(zvcmd):
         zero_volt_offset[j // 2, j % 2] = USBCtrlTrans(0x90, cmd, 1)
-
-    print('volt zero offset:\n', zero_volt_offset)
 
     #           A     B
     vscmd = [0xC2, 0xD2,  # 8
@@ -130,7 +132,8 @@ def read_volt_calib_data():
         volt_scale[j // 2, j % 2] = USBCtrlTrans(0x90, cmd, 1)
     volt_scale = volt_scale * 2 / 255
 
-    volt_scale[1, :] = [1.0333, 1.0079]
+    print('Internal calibration data:')
+    print('volt offset: \n', zero_volt_offset)
     print('volt scaling:\n', volt_scale)
 
     return zero_volt_offset, volt_scale
@@ -140,27 +143,22 @@ def user_volt_calib_data(raw_volt_offset_list = None, raw_volt_scale_list = None
     # Final volt_true = (volt_raw - volt_offset) * (v_range * 2 / 255 * volt_scale)
 
     # user calibration point
-    
-    ## version 1
-    j = 3 # 5V chA
-    v_range = osc_volt_series[j // 2]
-
     calib_data_array = np.array([
         # v_ref0, v_raw0, v_ref1, v_raw0
-        [0.0, 135.97, 8.000, 261.99],     # 8V chA
-        [0.0, 132.78, 8.000, 257.83],     # 8V chB
-        [0.0, 136.91, 3.210, 220.18],     # 5V chA
-        [0.0, 133.08, 5.000, 259.58],     # 5V chB
-        [0.0, 139.67, 2.500, 274.02],     # 2.5V chA
-        [0.0, 134.49, 2.500, 268.84],     # 2.5V chB
-        [0.0, 140.38, 1.000, 226.85],     # 1V chA
-        [0.0, 135.27, 1.000, 221.74],     # 1V chB
-        [0.0, 136.95, 0.500, 262.00],     # 0.5V chA
-        [0.0, 133.09, 0.500, 257.18],     # 0.5V chB
-        [0.0, 138.82, 0.250, 270.98],     # 0.25V chA
-        [0.0, 134.46, 0.250, 266.62],     # 0.25V chB
-        [0.0, 140.64, 0.100, 226.65],     # 0.1V chA
-        [0.0, 135.22, 0.100, 220.33],     # 0.1V chB
+        [0.0, 135.97,  4.722, 196.14],     # 8V chA
+        [0.0, 132.78,  4.722, 192.54],     # 8V chB
+        [0.0, 136.91,  3.144, 218.78],     # 5V chA
+        [0.0, 133.08,  3.144, 214.90],     # 5V chB
+        [0.0, 138.63,  1.577, 224.49],     # 2.5V chA
+        [0.0, 134.49,  1.577, 220.34],     # 2.5V chB
+        [0.0, 140.36, 0.7990, 210.84],     # 1V chA
+        [0.0, 135.23, 0.7990, 205.65],     # 1V chB
+        [0.0, 136.90, 0.4494, 250.97],     # 0.5V chA
+        [0.0, 133.01, 0.4494, 246.56],     # 0.5V chB
+        [0.0, 138.84, 0.1590, 223.14],     # 0.25V chA
+        [0.0, 134.06, 0.1590, 218.30],     # 0.25V chB
+        [0.0, 140.73,0.07969, 209.06],     # 0.1V chA
+        [0.0, 135.00,0.07969, 202.96],     # 0.1V chB
     ])
 
     v_range = np.vstack([osc_volt_series, osc_volt_series]).T.flatten()
@@ -174,10 +172,11 @@ def user_volt_calib_data(raw_volt_offset_list = None, raw_volt_scale_list = None
     #assert abs((v_raw0 - volt_offset) * (v_range * 2 / 255 * volt_scale) - v_ref0) < 1e-10
     #assert abs((v_raw1 - volt_offset) * (v_range * 2 / 255 * volt_scale) - v_ref1) < 1e-10
 
-    print('volt_offset:\n', raw_volt_offset_list)
-    print('volt_scale: \n', raw_volt_scale_list)
+    print('User calibration:')
+    print('volt_offset:\n', raw_volt_offset_list.reshape(-1, 2))
+    print('volt_scale: \n', raw_volt_scale_list.reshape(-1, 2))
 
-    return raw_volt_offset_list, raw_volt_scale_list
+    return raw_volt_offset_list.reshape(-1, 2), raw_volt_scale_list.reshape(-1, 2)
 
 class OSCA02Reader(tssabc.SampleReader):
     
@@ -191,12 +190,12 @@ class OSCA02Reader(tssabc.SampleReader):
 
         ## 1. set oscilloscope device model
         SpecifyDevIdx(c_int(6))            # 6: OSCA02
-        logger.info("1. 设置当前示波器设备编号为6(OSCA02)!")
+        logger.debug("1. 设置当前示波器设备编号为6(OSCA02)!")
 
         ## 2. open device
         OpenRes = DeviceOpen()
         if OpenRes == 0:
-            logger.info("2. 示波器设备连接 成功!")
+            logger.debug("2. 示波器设备连接 成功!")
         else:
             logger.error("2. 示波器设备连接 失败!")
             sys.exit(0)
@@ -207,7 +206,7 @@ class OSCA02Reader(tssabc.SampleReader):
             logger.error("GetBuffer Failed!")
             sys.exit(0)
         else:
-            logger.info("3. 获取数据缓冲区首地址 成功")
+            logger.debug("3. 获取数据缓冲区首地址 成功")
 
         # record IO control bits
         self.g_CtrlByte0 = 0
@@ -221,11 +220,11 @@ class OSCA02Reader(tssabc.SampleReader):
         # set trigger position at 50% of the buffer
         USBCtrlTrans(c_ubyte(0x18), c_ushort(0xff), c_ulong(1))
         USBCtrlTrans(c_ubyte(0x17), c_ushort(0x7f), c_ulong(1))
-        logger.info("X: 在步骤3和4之间，初始化硬件触发, 如果触发出问题, 请注释掉这个方法不调用")
+        logger.debug("X: 在步骤3和4之间，初始化硬件触发, 如果触发出问题, 请注释掉这个方法不调用")
 
         ## 4. set buffer size to 128KB, e.g. 64KB per channel
         SetInfo(c_double(1),  c_double(0),  c_ubyte(0x11),  c_int(0),   c_uint(0),  c_uint(self.chunk_size) )
-        logger.info("4. 设置使用的缓冲区为128K字节, 即每个通道64K字节")
+        logger.debug("4. 设置使用的缓冲区为128K字节, 即每个通道64K字节")
 
         ## 5. set oscilloscope sampling rate to 781kHz
         self.sampling_rate, self.g_CtrlByte0 = sampling_rate_normalization(sample_rate, self.g_CtrlByte0)
@@ -234,7 +233,7 @@ class OSCA02Reader(tssabc.SampleReader):
             logger.error("5. error")
             sys.exit(0)
         else:
-            logger.info("5. 设置示波器采样率781Khz")
+            logger.debug("5. 设置示波器采样率781Khz")
         
         # Enable channel B
         time.sleep(0.1)
@@ -266,7 +265,8 @@ class OSCA02Reader(tssabc.SampleReader):
 
         self.volt_a_max, self.volt_b_max, self.volt_ia, self.volt_ib, self.g_CtrlByte1 = \
             set_volt_range(volt_a, volt_b, self.g_CtrlByte1)
-        self.volt_offsets, self.volt_scale = read_volt_calib_data()
+        #self.volt_offsets, self.volt_scale = read_volt_calib_data()
+        self.volt_offsets, self.volt_scale = user_volt_calib_data()
         self.volt_a_offset = self.volt_offsets[self.volt_ia, 0]
         self.volt_b_offset = self.volt_offsets[self.volt_ib, 1]
         self.volt_a_scale = self.volt_scale[self.volt_ia, 0]
@@ -282,26 +282,30 @@ class OSCA02Reader(tssabc.SampleReader):
             self.f_volt_chb = lambda vb_bytes: \
                 (np.float64(vb_bytes) - self.volt_b_offset) * (2 * self.volt_b_max / 255)
 
+        print(f'volt_a_max: {self.volt_a_max:.2f}, volt_b_max: {self.volt_b_max:.2f}')
+        print(f'volt_a_offset: {self.volt_a_offset:.2f}, volt_b_offset: {self.volt_b_offset:.2f}')
+        print(f'volt_a_scale: {self.volt_a_scale:.4f}, volt_b_scale: {self.volt_b_scale:.4f}')
+
         ## 7. set AC/DC channel coupling
         self.g_CtrlByte0 &= 0xef # chA DC coupling
         self.g_CtrlByte0 |= 0x10 # chA 0x10:DC coupling, 0x00 AC coupling
         USBCtrlTrans(c_ubyte(0x94),  c_ushort(self.g_CtrlByte0),  c_ulong(1))
-        logger.info("7. 设置通道交直流耦合 设置chA为DC耦合")
+        logger.debug("7. 设置通道交直流耦合 设置chA为DC耦合")
         time.sleep(0.1)
 
         self.g_CtrlByte1 &= 0xef # chB DC coupling
         self.g_CtrlByte1 |= 0x10 # chB DC coupling
         Transres = USBCtrlTrans(c_ubyte(0x24),  c_ushort(self.g_CtrlByte1),  c_ulong(1))
-        logger.info("7. 设置通道交直流耦合 设置chB为DC耦合")
+        logger.debug("7. 设置通道交直流耦合 设置chB为DC耦合")
 
         ## 8. set trigger mode as non-trigger
         USBCtrlTrans(c_ubyte(0xE7),  c_ushort(0x00),  c_ulong(1))
-        logger.info("8. 设置当前触发模式为 无触发")
+        logger.debug("8. 设置当前触发模式为 无触发")
 
         if 0:
             # 8. trigger on rising edge, or turn off green LED
             USBCtrlTrans(c_ubyte(0xC5),  c_ushort(0x00),  c_ulong(1))
-            logger.info("8. 上升沿，或者设置LED绿色灯灭")
+            logger.debug("8. 上升沿，或者设置LED绿色灯灭")
         time.sleep(0.1)
 
         return self
@@ -309,7 +313,7 @@ class OSCA02Reader(tssabc.SampleReader):
     def read(self, n_frames = None):
         ## 9. start data acquisition
         USBCtrlTransSimple(c_ulong(0x33))
-        logger.info("9. 控制设备开始AD采集")
+        logger.debug("9. 控制设备开始AD采集")
         t1 = time.time()
 
         time_sample = self.chunk_size // 2 / self.sampling_rate
@@ -326,16 +330,16 @@ class OSCA02Reader(tssabc.SampleReader):
             logger.error("10. 缓冲区数据没有蓄满(查询结果)")
             sys.exit(0)
         else:
-            logger.info("10. 缓冲区数据已经蓄满(查询结果)")
+            logger.debug("10. 缓冲区数据已经蓄满(查询结果)")
 
         rBulkRes = AiReadBulkData( c_ulong(64 * 1024 * 2) ,  c_uint(1),  c_ulong(2000) , self.g_pBuffer,  c_ubyte(0),  c_uint(0))
         if 0 == rBulkRes:
-            logger.info("11. 传输获取采集的数据 成功!")
+            logger.debug("11. 传输获取采集的数据 成功!")
         else:
             logger.error("11. 传输获取采集的数据 失败!")
             #sys.exit(0)
 
-        logger.info("X: waiting data transmition... ")
+        logger.debug("X: waiting data transmition... ")
         ret = EventCheck(c_int(int(timeout_ms)))  # cost about 10~17ms
         t2 = time.time()
         if -1 == ret:
@@ -360,9 +364,9 @@ class OSCA02Reader(tssabc.SampleReader):
     def read_sleep(self, n_frames = None):
         ## 9. start data acquisition
         USBCtrlTransSimple(c_ulong(0x33))
-        logger.info("9. 控制设备开始AD采集")
+        logger.debug("9. 控制设备开始AD采集")
 
-        logger.info("X: sleep200ms 等待采集.... ")
+        logger.debug("X: sleep200ms 等待采集.... ")
         time.sleep(0.200)
 
         ## 10. check if data acquisition and storage is complete, if so, return 33
@@ -371,13 +375,13 @@ class OSCA02Reader(tssabc.SampleReader):
             logger.error("10. 缓冲区数据没有蓄满(查询结果)")
             sys.exit(0)
         else:
-            logger.info("10. 缓冲区数据已经蓄满(查询结果)")
+            logger.debug("10. 缓冲区数据已经蓄满(查询结果)")
 
         ## 11. get samples, where the size is determined by SetInfo()
         rBulkRes = AiReadBulkData( c_ulong(64 * 1024 * 2) ,  c_uint(1),  c_ulong(2000) , self.g_pBuffer,  c_ubyte(0),  c_uint(0))
 
         if 0 == rBulkRes:
-            logger.info("11. 传输获取采集的数据 成功!")
+            logger.debug("11. 传输获取采集的数据 成功!")
         else:
             logger.error("11. 传输获取采集的数据 失败!")
             sys.exit(0)
@@ -390,7 +394,7 @@ class OSCA02Reader(tssabc.SampleReader):
         return sample_d
 
     def close(self):
-        logger.info("closing device...")
+        logger.debug("closing device...")
         DeviceClose()
         self.initilized = False
 
@@ -417,7 +421,7 @@ if __name__ == '__main__':
         d = acq_dev.read()
         v = np.zeros_like(d, dtype=np.float32)
         v[:, 0] = acq_dev.f_volt_cha(d[:, 0])
-        v[:, 1] = acq_dev.f_volt_cha(d[:, 1])
+        v[:, 1] = acq_dev.f_volt_chb(d[:, 1])
         n_ignore = 100
 
         # plot the data in chA and chB in the same graph
@@ -442,12 +446,12 @@ if __name__ == '__main__':
             d = acq_dev.read()
             v = np.zeros_like(d, dtype=np.float32)
             v[:, 0] = acq_dev.f_volt_cha(d[:, 0])
-            v[:, 1] = acq_dev.f_volt_cha(d[:, 1])
+            v[:, 1] = acq_dev.f_volt_chb(d[:, 1])
             print(f'mean volt: {v[n_ignore:, 0].mean():.5g},'
                              f'{v[n_ignore:, 1].mean():.5g}')
             v_m_a = d[n_ignore:, 0].astype(np.float32).mean()
             v_m_b = d[n_ignore:, 1].astype(np.float32).mean()
-            if frame > 10:
+            if frame > 5:
                 set_v.append([v_m_a, v_m_b])
             print(f'mean byte: {v_m_a:.5g},'
                              f'{v_m_b:.5g}')
@@ -462,6 +466,9 @@ if __name__ == '__main__':
         #ani = animation.FuncAnimation(fig, update, frames=10, blit=True, repeat=False)  # run 10 frames
         plt.show()
 
-        print(np.array(set_v).mean(axis=0))
+        v_raw_mean = np.array(set_v).mean(axis=0)
+        print(f'v_raw:  {v_raw_mean[0]:.2f}  {v_raw_mean[1]:.2f}')
+        v_cal_mean = [acq_dev.f_volt_cha(v_raw_mean[0]), acq_dev.f_volt_chb(v_raw_mean[1])]
+        print(f'v_raw_cal:  {v_cal_mean[0]:.3f}  {v_cal_mean[1]:.3f}')
 
     acq_dev.close()
